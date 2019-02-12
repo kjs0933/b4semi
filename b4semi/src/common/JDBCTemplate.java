37c97e865ffc6711779dbfc8aaaf39f2106f2ef3
user2 <user2@user2-PC>
2019-02-12 20:39:31 +0900

    jdbctemplate

---
+++ b4semi/src/common/JDBCTemplate.java
@@ -0,0 +1,117 @@
+package common;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+
+public class JDBCTemplate {
+	
+	public static Connection getConnection()
+	{
+		Connection conn = null;
+		Properties prop = new Properties();
+		String fileName = JDBCTemplate.class.getResource("./driver.properties").getPath();
+		
+		try
+		{
+			prop.load(new FileReader(fileName));
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("pw"));
+			conn.setAutoCommit(false);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void close(Connection conn)
+	{
+		try
+		{
+			if(conn!=null && !conn.isClosed())
+			{
+				conn.close();
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt)
+	{
+		try
+		{
+			if(stmt!=null && !stmt.isClosed())
+			{
+				stmt.close();
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(ResultSet rs)
+	{
+		try
+		{
+			if(rs!=null && !rs.isClosed())
+			{
+				rs.close();
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void commit(Connection conn)
+	{
+		try
+		{
+			if(conn!=null && !conn.isClosed())
+			{
+				conn.commit();
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection conn)
+	{
+		try
+		{
+			if(conn!=null && !conn.isClosed())
+			{
+				conn.rollback();
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
