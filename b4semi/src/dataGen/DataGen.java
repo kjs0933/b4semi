d332b15477752ef305772e45f3142d2dd242aa16
Windows7 <Windows7@58.234.89.119>
2019-02-17 17:40:04 +0900

    장바구니, 일대일문의, 답변 생성 코드

--- b4semi/src/dataGen/DataGen.java
+++ b4semi/src/dataGen/DataGen.java
@@ -18,43 +18,41 @@ import common.JDBCTemplate;
 //더미 데이터 자동 생성을 위한 클래스 - DB 데이터 삭제 후 이용해주세요!
 public class DataGen {
 	
-	private static final int memberCount = 500;  //생성 회원수
+	private static final int memberCount = 500; //생성 회원수
 	private static final int noticeCount = 100; //공지사항 게시수
+	private static final int qnaCount = 2000; //일대일 문의 게시수
 	private static final long howOld = 31536000000L; //마켓 오픈시점과 현재의 밀리초 차이
 	private static final double priceInc = 0.2; //마켓 오픈시점부터 현시점까지의 물가상승률
 	
 	public static void main(String[] args) {
 
-		
 		int memberSeqStart = 1;
 		int dpListSeqStart = 1;
-
 	 
 		Connection cn = JDBCTemplate.getConnection();
 		
 		memberSeqStart = getMemberSeq(cn)+1;
 		
-		System.out.println("회원 생성중");
-		createMember(memberCount, memberSeqStart, cn); //매개변수 : 생성 인원수, 시퀀스 시작 번호, 커넥션
-		System.out.println("회원 생성 완료");
-		System.out.println("배송지 생성중");
-		createAddressList(memberCount, memberSeqStart, cn);
-		System.out.println("배송지 생성 완료");
-
+		//기존 데이터가 존재할 경우 초기화 로직
 		if(getProductCount(cn)>0)
 		{
-			System.out.println("상품 데이터 초기화중");
-			deleteDPOption(cn);
-			deleteProduct(cn);
+			System.out.println("상품 데이터 초기화중 - 에러 발생시 오라클에서 계정 삭제 후 다시 생성하여 초기화를 해주세요");
+			delete(cn);
+			System.out.println("상품 데이터 초기화 완료 - 재시작 필요");
 			JDBCTemplate.commit(cn);
-			System.out.println("상품 데이터 초기화 완료");
+			JDBCTemplate.close(cn);
+			return;
 		}
-		else
-		{
-			System.out.println("공급사 생성중");
-			createSupplier(cn);
-			System.out.println("공급사 생성 완료");
-		}
+		
+		System.out.println("회원 생성중");
+		createMember(memberSeqStart, cn); //매개변수 : 생성 인원수, 시퀀스 시작 번호, 커넥션
+		System.out.println("회원 생성 완료");
+		System.out.println("배송지 생성중");
+		createAddressList(memberSeqStart, cn);
+		System.out.println("배송지 생성 완료");
+		System.out.println("공급사 생성중");
+		createSupplier(cn);
+		System.out.println("공급사 생성 완료");
 		System.out.println("상품마스터 생성중");
 		ArrayList<ProductPrice> plist = ProductGen.createProduct(cn);
 		System.out.println("상품마스터 생성 완료");
@@ -65,16 +63,51 @@ public class DataGen {
 		System.out.println("판매세부옵션 생성중");
 		createDPOption(plist, dpListSeqStart, cn);
 		System.out.println("판매세부옵션 생성완료");
+		System.out.println("장바구니 생성중");
+		createBasket(plist, dpListSeqStart, memberSeqStart, cn);
+		System.out.println("장바구니 생성완료");
 		System.out.println("공지사항 생성중");
 		createNotice(plist, memberSeqStart, cn);
 		System.out.println("공지사항 생성 완료");
+		JDBCTemplate.commit(cn);
+		JDBCTemplate.close(cn);
+		cn = JDBCTemplate.getConnection();
+		System.out.println("일대일문의, 답변 생성중");
+		createQNA(plist, memberSeqStart, cn);
+		System.out.println("일대일문의, 답변 생성완료");
 		
 		
-
+		
 		JDBCTemplate.commit(cn);
 		JDBCTemplate.close(cn);
 
 	}
+	
+	public static int[] chooseIndex(int indexSize, int chooseCount)
+	{
+		if(indexSize<chooseCount)
+		{
+			return null;
+		}
+		
+		int[] nums = new int[chooseCount];
+		for(int i=0; i<chooseCount;i++)
+		{
+			nums[i] = (int)(Math.random()*indexSize);
+			for(int j=0; j<i;j++)
+			{
+				if(nums[i]==nums[j])
+				{
+					i--;
+					break;
+				}
+			}
+		}
+		return nums;
+		
+	}
+	
+
 	
 	public static int getMemberSeq(Connection cn)
 	{
@@ -107,7 +140,40 @@ public class DataGen {
 		return result;
 	}
 	
-	public static void createMember(int memberCount, int memberSeqStart, Connection cn)
+	
+	public static int getQnaSeq(Connection cn)
+	{
+		PreparedStatement ps =null;
+		ResultSet rs = null;
+		int result = 1;
+		try {
+			ps=cn.prepareStatement("SELECT QUERY_SEQ.NEXTVAL FROM DUAL");
+			rs=ps.executeQuery();
+			if(rs.next())
+			{
+				result = rs.getInt(1);
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try{
+				rs.close();
+				ps.close();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return result+1;
+	}
+	
+	
+	public static void createMember(int memberSeqStart, Connection cn)
 	{
 		
 		String[] firstNames = {"k강","k권","k김","n나","m문","p박","s손","s신","w왕","y유","y윤","l이","i임","j정","h한"};
@@ -186,7 +252,7 @@ public class DataGen {
 		}
 	}
 	
-	public static void createAddressList(int memberCount, int memberSeqStart, Connection cn)
+	public static void createAddressList(int memberSeqStart, Connection cn)
 	{
 		String[] address = AddressData.getAddress();
 		int addressSize = address.length;
@@ -309,6 +375,159 @@ public class DataGen {
 		
 	}
 	
+	public static void createQNA(ArrayList<ProductPrice> plist, int memberSeqStart, Connection cn)
+	{
+		long time;
+		int qnaWriter;
+		int replyWriter;
+		int qnaSeqStart = getQnaSeq(cn);
+		
+		String sql1 = "INSERT INTO QUERYBOARD VALUES (QUERY_SEQ.NEXTVAL,?,?,?,?,null)";
+		PreparedStatement ps1 =null;
+
+
+		for(int i=0; i<qnaCount;i++)
+		{
+			time = System.currentTimeMillis()-howOld+(long)((Math.random()+i)*(howOld/qnaCount));
+			qnaWriter = memberSeqStart+1 + (int)(Math.random()*(memberCount-1));
+			
+
+			try {
+
+				ps1=cn.prepareStatement(sql1);
+
+				//작성자 회원 번호
+				ps1.setInt(1,qnaWriter);
+				
+				//게시글 제목
+				if(Math.random()>0.5)
+				{
+					String date = new SimpleDateFormat("d일 H시").format(new Date(time+(long)(Math.random()*(howOld/qnaCount))));
+					//게시글 제목
+					ps1.setString(2, date + "에 배송 가능할까요?");
+					//게시글 내용
+					ps1.setString(3, "<img class='QNAImages' src='...imgpath...no.jpg'><br>꼭 부탁합니다");
+				}
+				else
+				{
+					if(Math.random()>0.5)
+					{
+						//게시글 제목
+						ps1.setString(2, plist.get((int)(plist.size()*Math.random())).getProductName() + " 상품에 대해 문의 드립니다");
+						//게시글 내용
+						ps1.setString(3, "<img class='NoticeImages' src='...imgpath...no.jpg'><br>깨끗하고 좋은 물건으로 보내주실 수 있으신가요");
+					}
+					else
+					{
+						String date = new SimpleDateFormat("M월 d일").format(new Date(time+(long)(Math.random()*(howOld/qnaCount))));
+						ProductPrice p = plist.get((int)(plist.size()*Math.random()));
+						//게시글 제목
+						ps1.setString(2, date + "에 구매한 " +p.getProductName() + " 환불에 대해 문의 드립니다");
+						//게시글 내용
+						ps1.setString(3, "<img class='NoticeImages' src='...imgpath...no.jpg'><br>받은 물건이 마음에 안 드는데 환불 가능한가요");
+					}
+				}
+
+				//작성일시
+				ps1.setTimestamp(4, new Timestamp(time));
+				ps1.executeUpdate();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try{
+					ps1.close();
+				}
+				catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			
+			if(Math.random()<0.05)
+			{
+				// 관리자 답변 없는 게시글
+				continue;
+			}
+			
+			String sql2 = "INSERT INTO REPLY VALUES (COMMENT_SEQ.NEXTVAL,?,?,null,?,?,1,null)";
+			PreparedStatement ps2=null;
+			
+			replyWriter=memberSeqStart;
+			
+			try {
+				ps2=cn.prepareStatement(sql2);
+				
+				for(;;)
+				{
+					time = time+(long)(Math.random()*Math.random()*(howOld/qnaCount));
+
+					//게시글 번호
+					ps2.setInt(1, qnaSeqStart);
+					
+					//작성자 회원 번호
+					ps2.setInt(2,replyWriter);
+					
+					//작성일시
+					ps2.setTimestamp(3, new Timestamp(time));
+
+					//댓글 내용
+					if(replyWriter==memberSeqStart)
+					{
+						String[] replyText = {"안녕하세요. The Food Forum 입니다.<br>좋은 하루 되세요.","안녕하세요. The Food Forum 입니다.<br>이용에 불편을 드려 죄송합니다.",
+								"안녕하세요. The Food Forum 입니다.<br>문의하신 내용 잘 확인하였습니다.","안녕하세요. The Food Forum 입니다.<br>문의해주신 대로 처리할 수 있도록 노력하겠습니다.",
+								"안녕하세요. The Food Forum 입니다.<br>해당 상품에 많은 관심 갖아주셔서 감사합니다."};
+						ps2.setString(4, replyText[(int)(Math.random()*replyText.length)]);
+					}
+					else
+					{
+						String[] replyText = {"이런","헐","알겠습니다","...","뭔말인지 잘 모르겠네요"};
+						ps2.setString(4, replyText[(int)(Math.random()*replyText.length)]);
+					}
+
+		
+					ps2.executeUpdate();
+
+
+					if(Math.random()>0.5)
+					{
+						if(Math.random()>0.75)
+						{
+							replyWriter=memberSeqStart;
+						}
+						else
+						{
+							replyWriter=qnaWriter;
+						}
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try{
+					ps2.close();
+				}
+				catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
 	public static void createSupplier(Connection cn) {
 		String[] address = AddressData.getAddress();
 		int addressSize = address.length;
@@ -408,15 +627,60 @@ public class DataGen {
 		
 	}
 	
-	public static void deleteProduct(Connection cn)
+	public static void delete(Connection cn)
 	{
+		PreparedStatement ps = null;
+		try {
+			ps=cn.prepareStatement("DELETE FROM CART CASCADE");
+			ps.executeUpdate();
+
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try{
+				ps.close();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		ps = null;
+		try {
+			ps=cn.prepareStatement("DELETE FROM DPOPTION CASCADE");
+			ps.executeUpdate();
+
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try{
+				ps.close();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
 		PreparedStatement ps1 = null;
 		PreparedStatement ps2 = null;
+		PreparedStatement ps3 = null;
 		try {
 			ps1=cn.prepareStatement("DELETE FROM PRODUCT CASCADE");
 			ps1.executeUpdate();
 			ps2=cn.prepareStatement("DELETE FROM IMAGES CASCADE");
 			ps2.executeUpdate();
+			ps3=cn.prepareStatement("DELETE FROM DPLIST CASCADE");
+			ps3.executeUpdate();
 
 		}
 		catch(SQLException e)
@@ -428,6 +692,7 @@ public class DataGen {
 			try{
 				ps1.close();
 				ps2.close();
+				ps3.close();
 
 			}
 			catch(SQLException e)
@@ -435,14 +700,11 @@ public class DataGen {
 				e.printStackTrace();
 			}
 		}
-	}
-	
-	public static void deleteDPOption(Connection cn)
-	{
-		PreparedStatement ps3 = null;
+		
+		ps = null;
 		try {
-			ps3=cn.prepareStatement("DELETE FROM DPOPTION CASCADE");
-			ps3.executeUpdate();
+			ps=cn.prepareStatement("DELETE FROM SUPPLIER CASCADE");
+			ps.executeUpdate();
 
 		}
 		catch(SQLException e)
@@ -452,13 +714,15 @@ public class DataGen {
 		finally
 		{
 			try{
-				ps3.close();
+				ps.close();
 			}
 			catch(SQLException e)
 			{
 				e.printStackTrace();
 			}
 		}
+		
+
 	}
 	
 	public static int getProductCount(Connection cn)
@@ -663,6 +927,50 @@ public class DataGen {
 		}
 	}
 	
-
+	public static void createBasket(ArrayList<ProductPrice> plist, int dpListSeqStart, int memberSeqStart, Connection cn)
+	{
+		PreparedStatement ps = null;
+		String sql = "INSERT INTO CART VALUES (?,?,?,?)";
+		
+		try {
+		ProductPrice p;
+		
+		int listSize = plist.size();
+		
+		ps=cn.prepareStatement(sql);
+		
+		int contain;
+		int[] pIndex;
+		
+			for(int i=memberSeqStart; i<memberSeqStart + memberCount; i++)
+			{
+				contain = 2;
+				pIndex = chooseIndex(listSize, contain);
+				for(int j=0; j<contain; j++)
+				{
+					p = plist.get(pIndex[j]);
+					ps.setInt(1, i);
+					ps.setString(2, p.getProductCode());
+					ps.setInt(3, dpListSeqStart+p.getDpNo());
+					ps.setInt(4, 1+(int)(Math.random()*20));
+					ps.executeUpdate();
+				}
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try{
+				ps.close();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
