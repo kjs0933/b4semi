7cb6cba918155a023de3bc966a8c59aeffda0fb5
user2 <user2@user2-PC>
2019-02-22 21:14:34 +0900

    할인명이 30자 이내라서 할인명 수정

--- b4semi/src/dataGen/DiscountGen.java
+++ b4semi/src/dataGen/DiscountGen.java
@@ -20,22 +20,22 @@ public class DiscountGen {
 		ps.executeQuery();
 		
 		ps.setString(1, "02F01-25");
-		ps.setString(2, "신선 과일 최대 25% 할인");
+		ps.setString(2, "과일 최대 25% 할인");
 		ps.setDouble(3, 0.25);
 		ps.executeQuery();
 		
 		ps.setString(1, "07C02-10");
-		ps.setString(2, "따뜻한 음식 & 필수품 모음전");
+		ps.setString(2, "따뜻한 음식 모음전");
 		ps.setDouble(3, 0.1);
 		ps.executeQuery();
 		
 		ps.setString(1, "04-20");
-		ps.setString(2, "삼삼해물 최대 20% 할인");
+		ps.setString(2, "해물 최대 20% 할인");
 		ps.setDouble(3, 0.2);
 		ps.executeQuery();
 		
 		ps.setString(1, "10H03-20");
-		ps.setString(2, "해통령 천연 분말 전 상품 20% 할인");
+		ps.setString(2, "천연 분말 20% 할인");
 		ps.setDouble(3, 0.2);
 		ps.executeQuery();
 		
